8649aed3f4860ff1341b58772ea7d629115970c0
karan734079 <karanjangral60@gmail.com>
2024-03-13 17:17:17 +0530

    commit

---
+++ src/components/About.js
@@ -0,0 +1,128 @@
+import React from "react";
+import { useState } from "react";
+
+function About(props) {
+    const [myStyle, setMyStyle] = useState({
+        color:"black",
+        backgroundColor:"white"
+    })
+
+    const [btnText, setbtnText] = useState("Enable Dark Mode")
+
+    const togglebtn=()=>{
+        if(myStyle.color === "white"){
+           setMyStyle({
+            color:"black",
+            backgroundColor:"white"
+        })
+        setbtnText("Enable Dark Mode");
+        }
+        else{
+            setMyStyle({
+                color:"white",
+                backgroundColor:"black"
+            })
+            setbtnText("Enable Light Mode");
+        }
+    }
+    return (
+        <div className="container" style={myStyle}>
+            <h3 className="my-3">{props.title}</h3>
+            <div className="accordion"  style={myStyle} id="accordionPanelsStayOpenExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button
+                            className="accordion-button"
+                            type="button"
+                            style={myStyle}
+                            data-bs-toggle="collapse"
+                            data-bs-target="#panelsStayOpen-collapseOne"
+                            aria-expanded="true"
+                            aria-controls="panelsStayOpen-collapseOne"
+                        >
+                            Accordion Item #1
+                        </button>
+                    </h2>
+                    <div
+                        id="panelsStayOpen-collapseOne"
+                        className="accordion-collapse collapse "
+                    >
+                        <div className="accordion-body" style={myStyle}>
+                            <strong>This is the first item's accordion body.</strong> It is
+                            shown by default, until the collapse plugin adds the appropriate
+                            classNamees that we use to style each element. These classNamees
+                            control the overall appearance, as well as the showing and hiding
+                            via CSS transitions. You can modify any of this with custom CSS or
+                            overriding our default variables. It's also worth noting that just
+                            about any HTML can go within the <code>.accordion-body</code>,
+                            though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            style={myStyle}
+                            data-bs-target="#panelsStayOpen-collapseTwo"
+                            aria-expanded="false"
+                            aria-controls="panelsStayOpen-collapseTwo"
+                        >
+                            Accordion Item #2
+                        </button>
+                    </h2>
+                    <div
+                        id="panelsStayOpen-collapseTwo"
+                        className="accordion-collapse collapse"
+                    >
+                        <div className="accordion-body" style={myStyle}>
+                            <strong>This is the second item's accordion body.</strong> It is
+                            hidden by default, until the collapse plugin adds the appropriate
+                            classNamees that we use to style each element. These classNamees
+                            control the overall appearance, as well as the showing and hiding
+                            via CSS transitions. You can modify any of this with custom CSS or
+                            overriding our default variables. It's also worth noting that just
+                            about any HTML can go within the <code>.accordion-body</code>,
+                            though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header">
+                        <button
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            style={myStyle}
+                            data-bs-target="#panelsStayOpen-collapseThree"
+                            aria-expanded="false"
+                            aria-controls="panelsStayOpen-collapseThree"
+                        >
+                            Accordion Item #3
+                        </button>
+                    </h2>
+                    <div
+                        id="panelsStayOpen-collapseThree"
+                        className="accordion-collapse collapse"
+                    >
+                        <div className="accordion-body" style={myStyle}>
+                            <strong>This is the third item's accordion body.</strong> It is
+                            hidden by default, until the collapse plugin adds the appropriate
+                            classNamees that we use to style each element. These classNamees
+                            control the overall appearance, as well as the showing and hiding
+                            via CSS transitions. You can modify any of this with custom CSS or
+                            overriding our default variables. It's also worth noting that just
+                            about any HTML can go within the <code>.accordion-body</code>,
+                            though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button className="btn btn-primary my-2" onClick={togglebtn}>{btnText}</button>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/TextForm.js
@@ -0,0 +1,124 @@
+import React, { useState, useCallback, useEffect } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+const TextForm = (props) => {
+  const [text, setText] = useState("");
+  const { listening, finalTranscript, resetTranscript } =
+    useSpeechRecognition();
+
+  const handleCliclUppercase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleClicklowerecase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearText = () => {
+    setText("");
+  };
+
+  const handleOnchange = (event) => {
+    setText(event.target.value);
+  };
+
+  const startListening = useCallback(() => {
+    SpeechRecognition.startListening({ continuous: true });
+  }, []);
+
+  const stopListening = useCallback(() => {
+    SpeechRecognition.stopListening();
+  }, []);
+
+  const handleReset = useCallback(() => {
+    resetTranscript();
+    setText("");
+  }, [resetTranscript]);
+
+  const handleListen = useCallback(() => {
+    if (listening) {
+      stopListening();
+    } else {
+      startListening();
+    }
+  }, [listening, startListening, stopListening]);
+
+  useEffect(() => {
+    if (finalTranscript) {
+      setText(finalTranscript); // Update text with finalTranscript
+    }
+  }, [finalTranscript]);
+
+  return (
+    <div
+      className="you"
+      style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+    >
+      <div className="mb-3">
+        <h1 className="my-4">{props.heading}</h1>
+        <textarea
+          className="form-control"
+          style={{
+            backgroundColor: props.mode === "dark" ? "#042743" : "white",
+            color: props.mode === "dark" ? "white" : "#042743",
+          }}
+          value={text}
+          id="Box"
+          placeholder="Enter Text Here"
+          onChange={handleOnchange}
+          rows="8"
+        />
+      </div>
+      <button
+        className="btn btn-primary text-white mx-1"
+        type="button"
+        onClick={handleCliclUppercase}
+      >
+        Convert To UPPERCASE
+      </button>
+      <button
+        className="btn btn-primary text-white mx-1"
+        type="button"
+        onClick={handleClicklowerecase}
+      >
+        Convert To lowercase
+      </button>
+      <button
+        className="btn btn-primary text-white"
+        type="button"
+        onClick={handleClearText}
+      >
+        Clear Text
+      </button>
+      <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>
+          {text.split(" ").length} Words and {text.length} Characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes To Read</p>
+        <h3>Preview</h3>
+        <p>{text.length>0?text:"Enter Something To TextBox Above To Preview Here"}</p>
+      </div>
+      <div className="speech-to-text-container">
+        <button
+          onClick={handleListen}
+          className="btn btn-primary text-white mx-1"
+        >
+          {listening ? "Stop" : "Start"} Listening
+        </button>
+        <button
+          onClick={handleReset}
+          className="btn btn-primary text-white mx-1"
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TextForm;
